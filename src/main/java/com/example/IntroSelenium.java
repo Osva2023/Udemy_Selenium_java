0a8b4a1eb3f58f717a1dba29b1997a9a812e1b52
Osvaldo Muguercia <elosvadel84@gmail.com>
2026-02-19 13:03:37 -0500

    Add IntroSelenium starter

---
+++ src/main/java/com/example/IntroSelenium.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IntroSelenium {
+    public static void main(String[] args) {
+        // Invoking the browser will go here.
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/");
+        try {
+            Thread.sleep(5000); // Wait for 5 seconds to see the browser
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String title = driver.getTitle();
+        System.out.println(title);
+        driver.quit();
+    }
+}
